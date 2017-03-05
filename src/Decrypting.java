19e0e5c0bea3653231112d53ac190e63de70a38b
Mateusz Bućko <mateuszbucko1@gmail.com>
2017-03-05 15:16:08 +0100

    CesarB decrypt + test

--- src/Decrypting.java
+++ src/Decrypting.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /**
  * Created by Mateusz Bućko on 03.03.2017.
  */
@@ -29,8 +31,15 @@ public class Decrypting {
     }
 
     public static String cesarDecryptB(String textToDecrypt,int key0,int key1){
-        //TODO: implementation
-        return null;
+        int fi_26 = 9;
+        StringBuilder decryptedText = new StringBuilder(textToDecrypt.length());
+        for(int i=0;i<textToDecrypt.length();i++) {
+            //TODO: change to BigInt for bigger keys value!!!!
+            int index = (Utils.ALPHABET.indexOf(textToDecrypt.charAt(i))+(Utils.ALPHABET.length() - key0))* (int) Math.pow(key1,fi_26-1) % Utils.ALPHABET.length();
+            decryptedText.append(Utils.ALPHABET.charAt(index));
+        }
+
+        return decryptedText.toString();
     }
 
     public static String vigenereDecrypt(String textToDecrypt, String key){
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        String text = "CRYPTOGRAPHY";
+
+        String cesarEncrypt = Encrypting.cesarEncryptB(text,7,3);
+
+        System.out.println(cesarEncrypt);
+
+        String cesarDecrypt = Decrypting.cesarDecryptB(cesarEncrypt,7,3);
+
+        System.out.println(cesarDecrypt);
 
     }
 }
